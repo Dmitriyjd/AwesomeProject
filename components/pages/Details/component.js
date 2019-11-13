b9d8f7b47efad032fdf86ece463c928b38b95087
Dmitriyjd <dmitry.bobr@cogniteq.com>
2019-11-13 14:28:31 +0300

    Initial commit

---
+++ components/pages/Details/component.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import { View } from 'react-native';
+
+class DetailsPage extends PureComponent {
+  render () {
+    return (
+      <View/>
+    )
+  }
+}
+
+export default DetailsPage
